51a470e9f5867f4473948c8a9d8c7455d6450104
Yauheni Shypila <yauheni_shypila@epam.com>
2021-01-15 13:53:19 +0300

    feat: implement Task 1. Introduction

---
+++ src/app/cart/enum/category.ts
@@ -0,0 +1,6 @@
+export enum category {
+  balls = 'balls',
+  snickers = 'snickers',
+  shorts = 'shorts',
+  socks = 'socks'
+}
---
+++ src/app/cart/products.ts
@@ -0,0 +1,10 @@
+import {category} from './enum/category';
+
+export const productsData = [
+  {name: 'KD11', price: 149, isAvailable: true, categories: category.snickers},
+  {name: 'Spalding 500', price: 99, isAvailable: false, categories: category.balls},
+  {name: 'Nike L', price: 14, isAvailable: true, categories: category.socks},
+  {name: 'KD12', price: 179, isAvailable: true, categories: category.snickers},
+  {name: 'Spalding 1000', price: 199, isAvailable: false, categories: category.balls},
+  {name: 'Nike S', price: 14, isAvailable: true, categories: category.socks}
+];
