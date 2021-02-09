3aa958401cc49846ba83c7b60da7998ab77c28ba
upskill js.basics <upskill.js.basics@gmail.com>
2021-02-09 17:39:04 +0200

    task4-comments

--- src/app/modules/shared/pipes/order-by.pipe.ts
+++ src/app/modules/shared/pipes/order-by.pipe.ts
@@ -7,14 +7,16 @@ import {ProductModel} from '../../products/models/product.model';
 export class OrderByPipe implements PipeTransform {
 
   transform(value: Array<ProductModel>, ...args: [string?, boolean?]): Array<ProductModel> {
-    const sortKey = args[0];
+    const [sortKey, order] = args;
+
     if (!sortKey) { return value; }
-    const order = args[1];
+
     if (order) { return value.sort((a, b) => {
-      if (a[sortKey] >= b[sortKey]) { return 1; }
-      if (a[sortKey] < b[sortKey]) { return -1; }
-    });
+        if (a[sortKey] >= b[sortKey]) { return 1; }
+        if (a[sortKey] < b[sortKey]) { return -1; }
+      });
     }
+
     return value.sort((a, b) => {
       if (a[sortKey] <= b[sortKey]) { return 1; }
       if (a[sortKey] > b[sortKey]) { return -1; }
