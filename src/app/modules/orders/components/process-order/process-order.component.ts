31f2093a6f6176071d89feda373e919e44ade753
Yauheni Shypila <yauheni_shypila@epam.com>
2021-04-05 13:39:49 +0300

    feat: implement Task 8 ( Forms  )

--- src/app/modules/orders/components/process-order/process-order.component.ts
+++ src/app/modules/orders/components/process-order/process-order.component.ts
@@ -1,6 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import {log} from "util";
+import {
+    AbstractControl,
+    FormArray,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    ValidationErrors,
+    Validators
+} from '@angular/forms';
+import {Observable} from "rxjs";
+import { of } from 'rxjs';
+import {debounceTime} from "rxjs/operators";
 
 @Component({
     selector: 'app-process-order',
@@ -10,26 +20,35 @@ import {log} from "util";
 export class ProcessOrderComponent implements OnInit {
 
     orderForm: FormGroup
+    isValid: boolean
 
     constructor(private fb: FormBuilder) {
     }
 
     ngOnInit(): void {
         this.orderForm = this.fb.group({
-            firstName: ['',[Validators.required,]],
-            lastName: ['',[Validators.required,]],
-            email: [''],
-            phones: this.fb.array([[]]),
+            firstName: ['', [Validators.required,]],
+            lastName: ['', ],
+            email: ['',[Validators.required,Validators.email]],
+            phones: this.fb.array([['']]),
             isPickUp: [false],
             deliveryAddress: [''],
-        });
+        },{ validators: this.isPickUpValidator });
 
         this.orderForm.statusChanges.subscribe(v => {
             console.log(v)
+            this.isValid=(v === 'VALID')
         })
 
     }
 
+    isPickUpValidator(control: AbstractControl): ValidationErrors | null {
+        const isPickUp = control.get('isPickUp');
+        const deliveryAddress = control.get('deliveryAddress');
+        if (isPickUp.value === false) {
+        return deliveryAddress.value.length? null : {isPickUpValidatorMessage: 'error'}
+        }
+    };
 
     get phones(): FormArray {
         return this.orderForm.get('phones') as FormArray;
@@ -39,7 +58,15 @@ export class ProcessOrderComponent implements OnInit {
         return this.orderForm.get('firstName') as FormControl;
     }
 
-    get isPickUp():boolean {
+    get email(): FormControl {
+        return this.orderForm.get('email') as FormControl;
+    }
+
+    get deliveryAddress(): FormControl {
+        return this.orderForm.get('deliveryAddress') as FormControl;
+    }
+
+    get isPickUp(): boolean {
         return this.orderForm.get('isPickUp').value;
     }
 
